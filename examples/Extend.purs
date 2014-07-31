6421ba535bcebf10201aa05cc452194ea21d1ec2
joneshf <jones3.hardy@gmail.com>
2014-07-31 13:06:17 -0700

    Use EasyFFI to make it less gross looking.

--- examples/Extend.purs
+++ examples/Extend.purs
@@ -1,21 +1,17 @@
 module Control.Lens.Examples.Extend where
 
-  import Control.Lens ((.~), (^.), lens, Lens(), LensP(), Setter())
+  import Control.Lens ((.~), (^.), lens, Lens(), Setter())
 
-  import Data.Function (runFn3, Fn3())
+  import Data.Foreign.EasyFFI (unsafeForeignFunction)
   import Debug.Trace (trace)
 
+  ffi = unsafeForeignFunction
+
   fooExt :: forall a r. Setter { | r} {foo :: a | r} Unit a
-  fooExt = lens (const unit) (runFn3 ext "foo")
+  fooExt = lens (const unit) (ffi ["obj", "x"] "obj.foo = x, obj")
 
   foo :: forall a b r. Lens {foo :: a | r} {foo :: b | r} a b
   foo = lens (\o -> o.foo) (\o x -> o{foo = x})
-
-  foreign import ext
-    "function ext(key, obj, val) {\
-    \  obj[key] = val;\
-    \  return obj;\
-    \}" :: forall a r r'. Fn3 String { | r} a { | r'}
 
   main = do
     trace $ (fooExt.~"wat") {bar: 3} ^. foo
